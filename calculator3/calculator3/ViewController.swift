6aed0dc1daccc8445961379d80646c2b09812a0a
aori <user@USER-no-MacBook.lan>
2015-05-10 16:18:59 +0900

    first commit

---
+++ calculator3/calculator3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calculator3
+//
+//  Created by USER on 2015/05/10.
+//  Copyright (c) 2015年 USER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
